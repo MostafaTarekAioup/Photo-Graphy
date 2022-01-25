3c511e98f1c9d0af871190418cd1b88363e41fdf
Mostafa Aioup <mostafatarekaioup@gmail.com>
2022-01-25 17:00:08 +0200

    Update ImageDetailsComponent.js

--- src/components/Image-details-component/ImageDetailsComponent.js
+++ src/components/Image-details-component/ImageDetailsComponent.js
@@ -9,7 +9,7 @@ const ImageDetailsComponent = () => {
     const isLoading = useSelector((state)=> state.imageComponent.isLoading)
     const imageId = useSelector((state)=> state.imageComponent.photoId)
     const imageData = useSelector((state)=> state.imageComponent.imageData)
-    const apiAccessKey = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+    const apiAccessKey = `?client_id=b8bf507eb41946edb729b42140589bee5682c6bee8b04498bf2686b6aaddd610`
     const dispatch = useDispatch()
 
     //fetch image data from API
